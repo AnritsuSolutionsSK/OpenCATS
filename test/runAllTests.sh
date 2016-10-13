6acb7eb9a38fc3c8f0cac6c23f7e1ed28ecc9593
skrchnavy <svetozar.krchnavy@anritsu.com>
2016-10-13 17:21:55 +0200

    small updates in behat tests related to config.php

--- test/runAllTests.sh
+++ test/runAllTests.sh
@@ -2,9 +2,11 @@
 cd /var/www/public/
 dockerize -wait tcp://opencats_test_mariadb:3306 -wait http://opencats_test_web:80 -timeout 30s
 php modules/tests/waitForDb.php
+ln -sf test_config.php config.php
 cat config.php
 ./vendor/bin/phpunit src/OpenCATS/Tests/IntegrationTests
 ./vendor/bin/behat -v -c ./test/behat.yml --suite="default"
 ./vendor/bin/behat -v -c ./test/behat.yml --suite="security"
-ln -sf demo_config.php config.php
+ln -sf test_demo_config.php config.php
+cat config.php
 ./vendor/bin/behat -v -c ./test/behat.yml --suite="demo"
